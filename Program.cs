1a212d1ff87fab8e84914d2d565af0c68933fc19
Artakhovhannisyan <39839190+Artakhovhannisyan@users.noreply.github.com>
2019-08-16 17:54:44 +0400

    Read directory and file names

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+public static class DirectoryTraverserDFS
+{
+    private static void TraverseDir(DirectoryInfo direction, string spaces)
+    {
+        Console.WriteLine(spaces + direction.FullName);
+
+        DirectoryInfo[] files = direction.GetDirectories();
+        FileInfo[] fileInfo;
+        foreach (DirectoryInfo file in files)
+        {
+            fileInfo = file.GetFiles();
+            Console.WriteLine("Files");
+            foreach ( FileInfo item in fileInfo)
+            {
+                Console.WriteLine(item.Name);
+            }
+            TraverseDir(file, spaces + "-");
+        }
+        
+    }
+
+    public static void TraverseDir(string directoryPath)
+    {
+        TraverseDir(new DirectoryInfo(directoryPath), "%");
+    }
+
+    public static void Main()
+    {
+        string path;
+        do
+        {
+            Console.Write("Path Name:");
+            path = Console.ReadLine();
+        }
+        while (!Directory.Exists($@"D:\{path}"));
+        
+       TraverseDir($@"D:\{path}");
+        
+        Console.ReadKey();
+    }
+}
